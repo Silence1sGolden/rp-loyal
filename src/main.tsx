39223731f69be415bce3c1ff84924f9f27aefb13
Danil <posikunets@yandex.ru>
2025-02-23 01:25:02 +0300

    fix react-router in GitHup Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
