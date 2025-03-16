a89f89a9748f941261cfdb3d61be03c5c57a9cc1
Danil <posikunets@yandex.ru>
2025-03-16 19:33:18 +0300

    фиксация мейна

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -13,5 +14,5 @@ export default defineConfig({
     ],
   },
   base: '/rp-loyal',
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
