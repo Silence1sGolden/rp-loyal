a9fb0e2ebff2ea18bd383c024ba0efc0ee2102d4
Danil <posikunets@yandex.ru>
2025-03-16 22:11:24 +0300

    написание нового слайса данных profile, запросы и мокс

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,18 @@
+import { RoleFormProps } from '@/components/ui/components/RoleForm/types';
+import { UserStats } from '@/components/ui/components/Stats/types';
+
 export interface User {
   username: string;
   email: string;
   profileIMG: string;
 }
+
+export type TUserProfileData = {
+  stats: UserStats;
+  likesTags: string[];
+  about: string;
+  rolesForms: RoleFormProps[];
+};
 
 export interface IUserData {
   emailValue: string;
